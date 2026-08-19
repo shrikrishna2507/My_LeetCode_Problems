bf2b336327a60f3f0a4a3a5caf1acd1a396153ba
shrikrishna2507 <shrikrishnas2005@gmail.com>
2026-08-19 20:49:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.95%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -6,14 +6,13 @@ class Solution(object):
         """
         curr = n
         while True:
-            # Calculate the product of the digits of curr
+          
             product = 1
             temp = curr
             while temp > 0:
                 product *= temp % 10
                 temp //= 10
             
-            # Check if the digit product is divisible by t
             if product % t == 0:
                 return curr
             
