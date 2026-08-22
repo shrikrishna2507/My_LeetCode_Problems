e1368cbae893769145c79920b965b8765a5e0fee
Shrikrishna S Bhat <shrikrishnas2005@gmail.com>
2026-08-22 19:56:01 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.53%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def checkDivisibility(self, n):
-        # Convert n to a string to access individual characters/digits
         n_str = str(n)
         
-        
-        s = sum(int(digit) for digit in n_str)
-        
+        s = 0
         p = 1
+        
         for digit in n_str:
-            p *= int(digit)
+            d = int(digit)
+            s += d
+            p *= d
             
-        if (n % (s + p) == 0):
+        if n % (s + p) == 0:
             return True
         else:
             return False
